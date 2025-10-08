2d9ea5b902d73b0b8cb894fc9e257dacc04d84f1
Hector13 <hectorhoracioaliende[200~~>
2025-10-08 13:39:39 -0300

    Fix start.sh and Procfile for Railway

--- start.sh
+++ start.sh
@@ -8,13 +8,9 @@ fi
 # Permisos necesarios
 chmod -R 775 storage bootstrap/cache
 
-# Cachear configuración, routes y views (opcional)
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Servir Laravel con PHP Built-in server en el puerto que asigna Railway
+# Servir Laravel con PHP Built-in server
 exec php -S 0.0.0.0:$PORT -t public
+
 
 
 
